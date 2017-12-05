850570747627939983870def16ad73f52fcd5c8b
Diogo Alves <diogo.alves@ancine.gov.br>
2017-12-05 15:31:03 -0200

    Adiciona listagem participantes recebida em 05/12/2017 11:14

--- src/participantes.js
+++ src/participantes.js
@@ -45,6 +45,11 @@ const pareceristas = [
     Nome: 'Ana Daniela de Souza Gillone'
   },
   {
+    Inscrição: 263,
+    CPF: '***.916.087-**',
+    Nome: 'ANA KEISERMAN DE ABREU'
+  },
+  {
     Inscrição: 140,
     CPF: '***.805.148-**',
     Nome: 'Ana Ligia de Carlos'
@@ -95,6 +100,11 @@ const pareceristas = [
     Nome: 'Arnaldo Clementino de Morais Galvão Neto'
   },
   {
+    Inscrição: 238,
+    CPF: '***.769.949-**',
+    Nome: 'AUGUSTINHO PASKO'
+  },
+  {
     Inscrição: 38,
     CPF: '***.646.387-**',
     Nome: 'Barbara de Brito Antunes Lito de Almeida'
@@ -127,7 +137,7 @@ const pareceristas = [
   {
     Inscrição: 226,
     CPF: '***.387.322-**',
-    Nome: 'Carlos Eduardo de Gusmão Bessa'
+    Nome: 'Carlos Eduardo de GusmÃ£o Bessa'
   },
   {
     Inscrição: 271,
@@ -208,7 +218,701 @@ const pareceristas = [
     Inscrição: 113,
     CPF: '***.214.418-**',
     Nome: 'Claudio Rogerio Quintas dos Santos'
+  },
+  {
+    Inscrição: 196,
+    CPF: '***.437.717-**',
+    Nome: 'Cristiana Giustino'
+  },
+  {
+    Inscrição: 223,
+    CPF: '***.964.278-**',
+    Nome: 'Cristiane Iglesias Arenas'
+  },
+  {
+    Inscrição: 153,
+    CPF: '***.946.106-**',
+    Nome: 'Cristiane Pederiva'
+  },
+  {
+    Inscrição: 9,
+    CPF: '***.363.376-**',
+    Nome: 'Cristiano Abud Barbosa'
+  },
+  {
+    Inscrição: 144,
+    CPF: '***.937.506-**',
+    Nome: 'Daniel de Queiroz Soares'
+  },
+  {
+    Inscrição: 259,
+    CPF: '***.997.168-**',
+    Nome: 'Daniel Tadeu Obeid'
+  },
+  {
+    Inscrição: 174,
+    CPF: '***.707.050-**',
+    Nome: 'DANIELA GOUVEIA MENEGOTTO'
+  },
+  {
+    Inscrição: 65,
+    CPF: '***.439.510-**',
+    Nome: 'Davi Agrello Pretto'
+  },
+  {
+    Inscrição: 122,
+    CPF: '***.946.538-**',
+    Nome: 'Deborah Osborn Gomes Nogueira'
+  },
+  {
+    Inscrição: 34,
+    CPF: '***.357.887-**',
+    Nome: 'Diogo Magalhães Pereira dos Santos'
+  },
+  {
+    Inscrição: 16,
+    CPF: '***.359.846-**',
+    Nome: 'Eric Belhassen'
+  },
+  {
+    Inscrição: 82,
+    CPF: '***.776.267-**',
+    Nome: 'Erica de Freitas Carvalho'
+  },
+  {
+    Inscrição: 37,
+    CPF: '***.833.478-**',
+    Nome: 'Evandra de Jesus Laurenti'
+  },
+  {
+    Inscrição: 17,
+    CPF: '***.608.989-**',
+    Nome: 'Éverson José Faganello'
+  },
+  {
+    Inscrição: 199,
+    CPF: '***.705.101-**',
+    Nome: 'FABIO AUGUSTO MEIRA DE SOUSA'
+  },
+  {
+    Inscrição: 167,
+    CPF: '***.008.734-**',
+    Nome: 'Fábio de Barros Leal'
+  },
+  {
+    Inscrição: 168,
+    CPF: '***.755.307-**',
+    Nome: 'Fabio Regaleira Motta de Paiva'
+  },
+  {
+    Inscrição: 245,
+    CPF: '***.963.987-**',
+    Nome: 'Felipe Lopes de Faria'
+  },
+  {
+    Inscrição: 152,
+    CPF: '***.708.097-**',
+    Nome: 'Felippe Schultz Mussel'
+  },
+  {
+    Inscrição: 157,
+    CPF: '***.586.627-**',
+    Nome: 'FERNANDO CESAR DA SILVA ARAUJO'
+  },
+  {
+    Inscrição: 88,
+    CPF: '***.063.537-**',
+    Nome: 'Flávia de Freitas Guimarães'
+  },
+  {
+    Inscrição: 125,
+    CPF: '***.512.560-**',
+    Nome: 'Flavio Garcia da Rocha'
+  },
+  {
+    Inscrição: 185,
+    CPF: '***.450.598-**',
+    Nome: 'Francini Nicolau Barbosa de Gusmão'
+  },
+  {
+    Inscrição: 126,
+    CPF: '***.543.639-**',
+    Nome: 'Francisco Lima Guarnieri'
+  },
+  {
+    Inscrição: 229,
+    CPF: '***.764.937-**',
+    Nome: 'Frederico da Cruz Machado'
+  },
+  {
+    Inscrição: 90,
+    CPF: '***.746.294-**',
+    Nome: 'Gabriel Mascaro Seabra de Melo'
+  },
+  {
+    Inscrição: 169,
+    CPF: '***.826.078-**',
+    Nome: 'Gabriela Partiti Mariano'
+  },
+  {
+    Inscrição: 118,
+    CPF: '***.513.168-**',
+    Nome: 'Guilherme Roberto Motta de Souza'
+  },
+  {
+    Inscrição: 119,
+    CPF: '***.241.077-**',
+    Nome: 'Gustavo Corga Acioli Lopes'
+  },
+  {
+    Inscrição: 225,
+    CPF: '***.242.378-**',
+    Nome: 'Gustavo Soranz Gonçalves'
+  },
+  {
+    Inscrição: 143,
+    CPF: '***.631.197-**',
+    Nome: 'Gustavo Souto Scofano'
+  },
+  {
+    Inscrição: 197,
+    CPF: '***.048.338-**',
+    Nome: 'Hadija Chalupe da Silva'
+  },
+  {
+    Inscrição: 78,
+    CPF: '***.930.218-**',
+    Nome: 'Heitor Augusto'
+  },
+  {
+    Inscrição: 92,
+    CPF: '***.656.988-**',
+    Nome: 'Henrique de Godoy Del Picchia Zanoni'
+  },
+  {
+    Inscrição: 151,
+    CPF: '***.949.526-**',
+    Nome: 'Heverton Souza Lima'
+  },
+  {
+    Inscrição: 77,
+    CPF: '***.135.138-**',
+    Nome: 'Hidalgo Romero'
+  },
+  {
+    Inscrição: 200,
+    CPF: '***.106.048-**',
+    Nome: 'Iana Cossoy Paro'
+  },
+  {
+    Inscrição: 155,
+    CPF: '***.733.288-**',
+    Nome: 'Iara Helena Magalhães'
+  },
+  {
+    Inscrição: 135,
+    CPF: '***.922.327-**',
+    Nome: 'Isabel Martinez Artavia'
+  },
+  {
+    Inscrição: 123,
+    CPF: '***.092.158-**',
+    Nome: 'Jaime Lerner'
+  },
+  {
+    Inscrição: 142,
+    CPF: '***.434.008-**',
+    Nome: 'Janaína de Ávila Anau'
+  },
+  {
+    Inscrição: 69,
+    CPF: '***.051.530-**',
+    Nome: 'Jessica do Vale Luz'
+  },
+  {
+    Inscrição: 110,
+    CPF: '***.105.707-**',
+    Nome: 'Joana Guedes Nin Ferreira'
+  },
+  {
+    Inscrição: 249,
+    CPF: '***.298.491-**',
+    Nome: 'Joel Pizzini Filho'
+  },
+  {
+    Inscrição: 25,
+    CPF: '***.276.707-**',
+    Nome: 'JOELZITO ALMEIDA DE ARAÚJO'
+  },
+  {
+    Inscrição: 28,
+    CPF: '***.548.467-**',
+    Nome: 'Joema de Oliveira Martins Flarys'
+  },
+  {
+    Inscrição: 242,
+    CPF: '***.148.791-**',
+    Nome: 'José Duarte Seixo Barahona Fernandes'
+  },
+  {
+    Inscrição: 81,
+    CPF: '***.428.528-**',
+    Nome: 'Jose Roberto Neffa Sadek'
+  },
+  {
+    Inscrição: 156,
+    CPF: '***.958.887-**',
+    Nome: 'Julia de Almeida Maciel Levy Tavares'
+  },
+  {
+    Inscrição: 22,
+    CPF: '***.145.977-**',
+    Nome: 'Júlia Santos Vanini'
+  },
+  {
+    Inscrição: 215,
+    CPF: '***.195.398-**',
+    Nome: 'Juliana de Araujo Rojas'
+  },
+  {
+    Inscrição: 224,
+    CPF: '***.604.947-**',
+    Nome: 'Juliana Milheiro Cruz'
+  },
+  {
+    Inscrição: 193,
+    CPF: '***.305.367-**',
+    Nome: 'Juliana Wanderley Reis'
+  },
+  {
+    Inscrição: 75,
+    CPF: '***.815.658-**',
+    Nome: 'Julio de Matos Lima'
+  },
+  {
+    Inscrição: 264,
+    CPF: '***.523.897-**',
+    Nome: 'Leandro Matos'
+  },
+  {
+    Inscrição: 71,
+    CPF: '***.779.410-**',
+    Nome: 'Leandro Rocha Saraiva'
+  },
+  {
+    Inscrição: 186,
+    CPF: '***.798.807-**',
+    Nome: 'Leonardo Copello Pirovano'
+  },
+  {
+    Inscrição: 232,
+    CPF: '***.092.237-**',
+    Nome: 'Leonardo dos Guimarães Bittencourt'
+  },
+  {
+    Inscrição: 183,
+    CPF: '***.773.217-**',
+    Nome: 'Leonardo Levis'
+  },
+  {
+    Inscrição: 180,
+    CPF: '***.172.918-**',
+    Nome: 'Leonardo Mecchi'
+  },
+  {
+    Inscrição: 41,
+    CPF: '***.664.230-**',
+    Nome: 'Letícia de Cássia Costa de Oliveira'
+  },
+  {
+    Inscrição: 55,
+    CPF: '***.046.447-**',
+    Nome: 'Leticia Dornelles'
+  },
+  {
+    Inscrição: 160,
+    CPF: '***.099.837-**',
+    Nome: 'LIA BAHIA CESÁRIO'
+  },
+  {
+    Inscrição: 35,
+    CPF: '***.610.067-**',
+    Nome: 'Lis De Carvalho Kogan'
+  },
+  {
+    Inscrição: 243,
+    CPF: '***.207.338-**',
+    Nome: 'Lívia Perez de Paula'
+  },
+  {
+    Inscrição: 213,
+    CPF: '***.716.238-**',
+    Nome: 'Lucia Helena de Camargo'
+  },
+  {
+    Inscrição: 12,
+    CPF: '***.385.460-**',
+    Nome: 'Luciana Rodrigues Silva'
+  },
+  {
+    Inscrição: 30,
+    CPF: '***.129.238-**',
+    Nome: 'Luiz Alberto Mendes Pereira'
+  },
+  {
+    Inscrição: 13,
+    CPF: '***.228.407-**',
+    Nome: 'Luiz Ernesto Mendes Bretz'
+  },
+  {
+    Inscrição: 178,
+    CPF: '***.133.136-**',
+    Nome: 'Luiz Fernando Guedes Toste Verissimo'
+  },
+  {
+    Inscrição: 274,
+    CPF: '***.481.488-**',
+    Nome: 'Luiz Ferraz'
+  },
+  {
+    Inscrição: 148,
+    CPF: '***.278.588-**',
+    Nome: 'Marcela Lordy Costa'
+  },
+  {
+    Inscrição: 80,
+    CPF: '***.039.397-**',
+    Nome: 'Marcelo Pedrazzi Scudieri'
+  },
+  {
+    Inscrição: 97,
+    CPF: '***.627.904-**',
+    Nome: 'Marcelo Pedroso Holanda de Jesus'
+  },
+  {
+    Inscrição: 101,
+    CPF: '***.516.837-**',
+    Nome: 'Marcelo Rangel Lima'
+  },
+  {
+    Inscrição: 134,
+    CPF: '***.012.307-**',
+    Nome: 'Marcos Amarante de Almeida Magalhães'
+  },
+  {
+    Inscrição: 162,
+    CPF: '***.009.157-**',
+    Nome: 'Maria Clara Terra Escobar'
+  },
+  {
+    Inscrição: 139,
+    CPF: '***.326.467-**',
+    Nome: 'Mariana Carpenter Genescá'
+  },
+  {
+    Inscrição: 175,
+    CPF: '***.564.656-**',
+    Nome: 'Marília Rocha'
+  },
+  {
+    Inscrição: 114,
+    CPF: '***.409.477-**',
+    Nome: 'Mariza Helena de Figueiredo'
+  },
+  {
+    Inscrição: 272,
+    CPF: '***.960.118-**',
+    Nome: 'Martha Vicente de Azevedo Nowill'
+  },
+  {
+    Inscrição: 79,
+    CPF: '***.109.697-**',
+    Nome: 'Mauro Baptista Vedia'
+  },
+  {
+    Inscrição: 270,
+    CPF: '***.268.821-**',
+    Nome: 'Mauro Giuntini Viana'
+  },
+  {
+    Inscrição: 257,
+    CPF: '***.578.948-**',
+    Nome: 'Max Eluard Fernandes'
+  },
+  {
+    Inscrição: 51,
+    CPF: '***.212.018-**',
+    Nome: 'Melanie Dimantas'
+  },
+  {
+    Inscrição: 83,
+    CPF: '***.156.857-**',
+    Nome: 'Michael Greif'
+  },
+  {
+    Inscrição: 67,
+    CPF: '***.793.618-**',
+    Nome: 'Michael Wahrmann'
+  },
+  {
+    Inscrição: 255,
+    CPF: '***.653.874-**',
+    Nome: 'Milena Silvino Evangelista'
+  },
+  {
+    Inscrição: 260,
+    CPF: '***.345.077-**',
+    Nome: 'MOEMA MULLER NUNES DA CUNHA'
+  },
+  {
+    Inscrição: 18,
+    CPF: '***.439.728-**',
+    Nome: 'Monica Christine Hubert Zafita'
+  },
+  {
+    Inscrição: 269,
+    CPF: '***.076.307-**',
+    Nome: 'Mônica Frota Leão Feitosa'
+  },
+  {
+    Inscrição: 45,
+    CPF: '***.751.357-**',
+    Nome: 'Monica Portella'
+  },
+  {
+    Inscrição: 50,
+    CPF: '***.985.017-**',
+    Nome: 'MURIEL ALVES XAVIER ROCHA'
+  },
+  {
+    Inscrição: 70,
+    CPF: '***.251.698-**',
+    Nome: 'Natalia Christofoletti Barrenha'
+  },
+  {
+    Inscrição: 107,
+    CPF: '***.917.258-**',
+    Nome: 'Nikolas Vilarouca Maciel'
+  },
+  {
+    Inscrição: 258,
+    CPF: '***.072.468-**',
+    Nome: 'Orlando César Margarido'
+  },
+  {
+    Inscrição: 85,
+    CPF: '***.378.055-**',
+    Nome: 'Pablo Orrico'
+  },
+  {
+    Inscrição: 64,
+    CPF: '***.046.550-**',
+    Nome: 'Paola Wink Tedesco'
+  },
+  {
+    Inscrição: 120,
+    CPF: '***.882.804-**',
+    Nome: 'Patricia Alves Dias'
+  },
+  {
+    Inscrição: 252,
+    CPF: '***.759.216-**',
+    Nome: 'Patrícia Mourão de Andrade'
+  },
+  {
+    Inscrição: 181,
+    CPF: '***.921.148-**',
+    Nome: 'Paulo Celso Lui'
+  },
+  {
+    Inscrição: 233,
+    CPF: '***.108.059-**',
+    Nome: 'Paulo Roberto Munhoz'
+  },
+  {
+    Inscrição: 247,
+    CPF: '***.349.788-**',
+    Nome: 'Paulo Sacramento'
+  },
+  {
+    Inscrição: 39,
+    CPF: '***.759.367-**',
+    Nome: 'Paulo Sergio Almeida da Silva'
+  },
+  {
+    Inscrição: 261,
+    CPF: '***.049.587-**',
+    Nome: 'Pedro Butcher'
+  },
+  {
+    Inscrição: 154,
+    CPF: '***.297.871-**',
+    Nome: 'Pedro da Costa Novars'
+  },
+  {
+    Inscrição: 115,
+    CPF: '***.945.526-**',
+    Nome: 'Pedro Maciel Guimaraes Junior'
+  },
+  {
+    Inscrição: 91,
+    CPF: '***.736.964-**',
+    Nome: 'Rachel Daisy Clarke Ellis'
+  },
+  {
+    Inscrição: 198,
+    CPF: '***.362.610-**',
+    Nome: 'Ramiro Paiva de Azevedo'
+  },
+  {
+    Inscrição: 131,
+    CPF: '***.837.028-**',
+    Nome: 'Raul Lemos Arthuso'
+  },
+  {
+    Inscrição: 171,
+    CPF: '***.343.338-**',
+    Nome: 'Renato Coelho Pannacci'
+  },
+  {
+    Inscrição: 57,
+    CPF: '***.131.474-**',
+    Nome: 'Riccardo Migliore'
+  },
+  {
+    Inscrição: 137,
+    CPF: '***.567.156-**',
+    Nome: 'Rinaldo Santos Teixeira'
+  },
+  {
+    Inscrição: 104,
+    CPF: '***.432.435-**',
+    Nome: 'Rita Gorette Neri Randam'
+  },
+  {
+    Inscrição: 129,
+    CPF: '***.310.768-**',
+    Nome: 'Roberto Edgar Gervitz'
+  },
+  {
+    Inscrição: 170,
+    CPF: '***.620.117-**',
+    Nome: 'Roberval Duarte de Araújo'
+  },
+  {
+    Inscrição: 192,
+    CPF: '***.358.461-**',
+    Nome: 'Rodrigo Cássio Oliveira'
+  },
+  {
+    Inscrição: 149,
+    CPF: '***.623.267-**',
+    Nome: 'Rodrigo Sarti Werthein'
+  },
+  {
+    Inscrição: 47,
+    CPF: '***.444.988-**',
+    Nome: 'Rogério Zagallo Camargo'
+  },
+  {
+    Inscrição: 184,
+    CPF: '***.349.118-**',
+    Nome: 'Rossana Elisa Foglia'
+  },
+  {
+    Inscrição: 127,
+    CPF: '***.377.148-**',
+    Nome: 'Rune Tavares'
+  },
+  {
+    Inscrição: 96,
+    CPF: '***.974.887-**',
+    Nome: 'Sabrina de Lima e Silva Garcia'
+  },
+  {
+    Inscrição: 218,
+    CPF: '***.699.318-**',
+    Nome: 'Sabrina Nudeliman Wagon'
+  },
+  {
+    Inscrição: 158,
+    CPF: '***.660.103-**',
+    Nome: 'Salomão Santana Lima'
+  },
+  {
+    Inscrição: 105,
+    CPF: '***.513.676-**',
+    Nome: 'Sanzio Dolabela Canfora'
+  },
+  {
+    Inscrição: 189,
+    CPF: '***.716.226-**',
+    Nome: 'Sérgio Borges Martins Filho'
+  },
+  {
+    Inscrição: 244,
+    CPF: '***.062.800-**',
+    Nome: 'Simone Luz Ferreira Constante'
+  },
+  {
+    Inscrição: 49,
+    CPF: '***.668.667-**',
+    Nome: 'Simone Marçal'
+  },
+  {
+    Inscrição: 267,
+    CPF: '***.119.347-**',
+    Nome: 'SIMONE MELAMED'
+  },
+  {
+    Inscrição: 59,
+    CPF: '***.501.428-**',
+    Nome: 'Sonia Maria Achatkin'
+  },
+  {
+    Inscrição: 72,
+    CPF: '***.007.457-**',
+    Nome: 'Tânia Aparecida de Souza Vicente'
+  },
+  {
+    Inscrição: 52,
+    CPF: '***.076.588-**',
+    Nome: 'Thais Fujinaga'
+  },
+  {
+    Inscrição: 116,
+    CPF: '***.535.867-**',
+    Nome: 'Thereza Jessouroun'
+  },
+  {
+    Inscrição: 275,
+    CPF: '***.506.818-**',
+    Nome: 'Thiago Mendonça'
+  },
+  {
+    Inscrição: 172,
+    CPF: '***.003.604-**',
+    Nome: 'Ulisses de Freitas Xavier'
+  },
+  {
+    Inscrição: 141,
+    CPF: '***.059.647-**',
+    Nome: 'Vicente Ferraz'
+  },
+  {
+    Inscrição: 23,
+    CPF: '***.136.859-**',
+    Nome: 'Virginia Ganter Moraes'
+  },
+  {
+    Inscrição: 56,
+    CPF: '***.062.785-**',
+    Nome: 'Viviane Ferreira da Cruz'
+  },
+  {
+    Inscrição: 146,
+    CPF: '***.144.728-**',
+    Nome: 'Zita Cintra Gordinho Barros de Carvalhosa'
   }
 ];
-
 export default pareceristas;
